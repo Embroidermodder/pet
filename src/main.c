f71e8e401949dddb19a5f346d8eb656a378236b0
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2023-09-12 23:06:29 +0100

    Create LICENSE.md, README.md, some boilerplate setup

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the Embroidermodder PET.
+ *
+ * Copyright 2023 The Embroidermodder Team
+ * Licensed under the terms of the zlib license.
+ *
+ * ---------------------------------------------------------
+ *
+ * For now, all code not in libembroidery goes here.
+ */
+
+#include "embroidery.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
