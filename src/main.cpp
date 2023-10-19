1e3aafd4b6c9c8789d97cc0662b00808e0a5b800
Robin Swift <robinswiftart@gmail.com>
2023-10-19 14:18:57 +0100

    working example

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,90 @@
  * Licensed under the terms of the zlib license.
  *
  * ---------------------------------------------------------
- *
- * For now, all code not in libembroidery goes here.
  */
 
-#include "pico_explorer.hpp"
+#include "hardware/gpio.h"
 #include "drivers/st7789/st7789.hpp"
 #include "libraries/pico_graphics/pico_graphics.hpp"
+#include "tufty2040.hpp"
+
+#include "interface.h"
+
+int button_state[10];
 
 using namespace pimoroni;
 
-ST7789 st7789(PicoExplorer::WIDTH, PicoExplorer::HEIGHT, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
+Tufty2040 tufty;
+ST7789 st7789(
+    Tufty2040::WIDTH,
+    Tufty2040::HEIGHT,
+    ROTATE_0,
+    ParallelPins{
+        Tufty2040::LCD_CS,
+        Tufty2040::LCD_DC,
+        Tufty2040::LCD_WR,
+        Tufty2040::LCD_RD,
+        Tufty2040::LCD_D0,
+        Tufty2040::BACKLIGHT
+    }
+);
 PicoGraphics_PenRGB332 graphics(st7789.width, st7789.height, nullptr);
 
-int main() {
-    graphics.set_pen(255, 0, 0);
+//Button button_a(Tufty2040::A, Polarity::ACTIVE_HIGH);
 
-    while(true) {
-        graphics.pixel(Point(0, 0));
+/* Wrappers for PicoGraphics class functions for C. */
+void
+text(char *msg, int x, int y, int wordwrap)
+{
+    graphics.text(msg, Point(x, y), wordwrap);
+}
 
-        // now we've done our drawing let's update the screen
+void
+line(int x1, int y1, int x2, int y2)
+{
+    graphics.line(Point(x1, y1), Point(x2, y2));
+}
+
+void
+rectangle(int x, int y, int w, int h)
+{
+    graphics.rectangle(Rect(x, y, w, h));
+}
+
+void
+polygon(int *a, int length)
+{
+}
+
+void
+pixel(int x, int y)
+{
+    graphics.pixel(Point(x, y));
+}
+
+int
+main(void)
+{
+    Pen bg = graphics.create_pen(255, 0, 0);
+    Pen fg = graphics.create_pen(0, 0, 0);
+    st7789.set_backlight(255);
+    graphics.set_font("bitmap8");
+    gpio_init(Tufty2040::A);
+
+    while (true) {
+        graphics.set_pen(bg);
+        graphics.clear();
+        graphics.set_pen(fg);
+//        user_interface(button_state);
+//        pixel(0, 0);
+//        line(10, 20, 20, 20);
+//        text("hello world", 30, 40, 100);
+
+        if (gpio_get(Tufty2040::A) > 0) {
+           rectangle(10, 20, 20, 20);
+        }
+        graphics.pixel(Point(10, 10));
+        graphics.pixel(Point(20, 30));
         st7789.update(&graphics);
     }
 }
